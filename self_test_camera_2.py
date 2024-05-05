05f650a77030ffd271c1c4f593b5ddb60d4611b4
Chaoen <calvin19961211@gmail.com>
2024-05-05 22:35:36 +0800

    測試

--- self_test_camera_2.py
+++ self_test_camera_2.py
@@ -8,7 +8,7 @@ import numpy as np
 # ffmpeg -re -stream_loop -1 -i /home/chaoen/yoloNhit_calvin/yolov7/inference/videos_long/0010.mp4 -map 0:v -f v4l2 /dev/video0
 
 
-in_filename = "/home/siplab4/chaoen/yoloNhit_calvin/yolov7/inference/videos_long/6.mp4"
+in_filename = "./inference/videos/0005.mp4"
 
 decklink_name = "DeckLink Quad HDMI Recorder (3)"
 width = 1920
@@ -23,25 +23,33 @@ height = 1080
 
 
 process1 = (
-    ffmpeg
-    .input(in_filename)
-    .filter('fps', fps=60, round='up')
-    .output('pipe:', format='rawvideo', pix_fmt='rgb24')
+    ffmpeg.input(in_filename)
+    .filter("fps", fps=60, round="up")
+    .output("pipe:", format="rawvideo", pix_fmt="rgb24")
     .run_async(pipe_stdout=True)
 )
 
+# cap = cv2.VideoCapture("/dev/video0")
+
+# if not cap.isOpened():
+#     print("Error: Could not open video file.")
+#     exit()
 
 avg_time = 0.0
 t = time.time()
-while(True):
+while True:
     in_bytes = process1.stdout.read(width * height * 3)
     if not in_bytes:
         break
-    in_frame = (
-        np
-        .frombuffer(in_bytes, np.uint8)
-        .reshape([height, width, 3])
-    )
-    avg_time = avg_time * 0.5 + (time.time() - t) * 0.5
-    t = time.time()
-    # print(f"{1.0 / (avg_time):.1f} FPS")
+    in_frame = np.frombuffer(in_bytes, np.uint8).reshape([height, width, 3])
+
+    # ret, in_frame = cap.read()
+    # if not ret:
+    #     break
+
+    in_frame = cv2.cvtColor(in_frame, cv2.COLOR_RGB2BGR)
+    cv2.imshow("test", in_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+print(f"{time.time() - t} seconds")
