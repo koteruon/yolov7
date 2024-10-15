2ba542c0f54fef63f6ea2eb8b18378827545d8bb
Chaoen <calvin19961211@gmail.com>
2024-10-15 17:56:20 +0800

    增加發球機遮罩

--- detect.py
+++ detect.py
@@ -1,35 +1,61 @@
 import argparse
+import os
 import re
 import time
 from pathlib import Path
 
 import cv2
+import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
+from matplotlib.path import Path as matplotlib_path
 from numpy import random
 
 from keypoints_detection import KeyPointDetection
 from models.experimental import attempt_load
 from process_videos import ProcessVideos
 from utils.datasets import LoadCamera, LoadImages, LoadStreams
-from utils.general import (
-    apply_classifier,
-    check_img_size,
-    check_imshow,
-    check_requirements,
-    increment_path,
-    non_max_suppression,
-    scale_coords,
-    set_logging,
-    strip_optimizer,
-    xyxy2xywh,
-)
+from utils.general import (apply_classifier, check_img_size, check_imshow,
+                           check_requirements, increment_path,
+                           non_max_suppression, scale_coords, set_logging,
+                           strip_optimizer, xyxy2xywh)
 from utils.plots import plot_one_box
-from utils.torch_utils import TracedModel, load_classifier, select_device, time_synchronized
+from utils.torch_utils import (TracedModel, load_classifier, select_device,
+                               time_synchronized)
 
 
 class YoloV7:
-    def detect(self, save_img=False, only_ball=False):
+    def __init__(self):
+        self.has_mark_no_count_points = False
+    def Draw_Circle(self, event, x, y, flags, param):
+        # 用於透視變形取點
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            cv2.circle(param["img"], (x, y), 3, (0, 255, 255), -1)
+            param["point_x"].append(x)
+            param["point_y"].append(y)
+    def Mark_No_Count_Point(self, image, frame_width, frame_height):
+        # 點選透視變形位置, 順序為:左上,左下,右下,右上
+        PT_data = {"img": image.copy(), "point_x": [], "point_y": []}
+        # TODO 測試用
+        cv2.namedWindow("PIC2 (press Q to quit)", 0)
+        cv2.resizeWindow("PIC2 (press Q to quit)", frame_width, frame_height)
+        cv2.setMouseCallback("PIC2 (press Q to quit)", self.Draw_Circle, PT_data)
+        while True:
+            cv2.imshow("PIC2 (press Q to quit)", PT_data["img"])
+            if cv2.waitKey(2) == ord("q"):
+                print(PT_data)
+                cv2.destroyWindow("PIC2 (press Q to quit)")
+                break
+
+        # PerspectiveTransform
+        upper_left = [PT_data["point_x"][0] / frame_width, PT_data["point_y"][0] / frame_height]
+        lower_left = [PT_data["point_x"][1] / frame_width, PT_data["point_y"][1] / frame_height]
+        lower_right = [PT_data["point_x"][2] / frame_width, PT_data["point_y"][2] / frame_height]
+        upper_right = [PT_data["point_x"][3] / frame_width, PT_data["point_y"][3] / frame_height]
+        no_count_point = np.float32([upper_left, lower_left, lower_right, upper_right])
+        self.polygon_path = matplotlib_path(no_count_point)
+
+    def detect(self, save_ori_img=False, only_ball=False, mark_no_count=False):
         source, weights, view_img, save_txt, imgsz, trace = (
             opt.source,
             opt.weights,
@@ -90,6 +116,7 @@ class YoloV7:
         else:
             dataset = LoadImages(source, img_size=imgsz, stride=stride)
 
+
         # Get names and colors
         names = model.module.names if hasattr(model, "module") else model.names
         # colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
@@ -104,6 +131,10 @@ class YoloV7:
 
         t0 = time.time()
         for path, img, im0s, vid_cap in dataset:
+            if not self.has_mark_no_count_points and mark_no_count:
+                self.Mark_No_Count_Point(im0s, im0s.shape[1], im0s.shape[0])
+                self.has_mark_no_count_points = True
+
             img = torch.from_numpy(img).to(device)
             img = img.half() if half else img.float()  # uint8 to fp16/32
             img /= 255.0  # 0 - 255 to 0.0 - 1.0
@@ -151,6 +182,13 @@ class YoloV7:
                     im0_origin = im0.copy()
                     clip_path = str(save_dir / "clips" / "test" / p.stem / f"{frame}") + ".jpg"  # 0.jpg
                     keyframe_path = str(save_dir / "keyframes" / "test" / p.stem / f"{frame}") + ".jpg"  # 0.jpg
+                # Save original image
+                if save_ori_img:
+                    im0_origin = im0.copy()
+                    clip_path = str(save_dir / "clips" / "test" / p.stem / f"{frame}") + ".jpg"  # 0.jpg
+                    os.makedirs(os.path.dirname(clip_path), exist_ok=True)
+                    cv2.imwrite(clip_path, im0_origin)
+
                 txt_path = str(save_dir / "labels" / p.stem) + (
                     "" if dataset.mode == "image" else f"_{frame}"
                 )  # img.txt
@@ -184,6 +222,9 @@ class YoloV7:
                                 numerator, denominator = map(int, opt.ball_botton_boundary.split("/"))
                                 if xywh[1] > (numerator / denominator):  # y軸在界線之下
                                     continue
+                            ball_center = [xywh[0], xywh[1]]
+                            if self.has_mark_no_count_points and self.polygon_path.contains_point(ball_center):
+                                continue
                         if cls == 1:  # person boundaries
                             if opt.person_left_boundary != "" and opt.person_right_boundary != "":
                                 left_numerator, left_denominator = map(int, opt.person_left_boundary.split("/"))
@@ -305,6 +346,7 @@ if __name__ == "__main__":
     )
     parser.add_argument("--save-txt", action="store_true", help="save results to *.txt")
     parser.add_argument("--save-conf", action="store_true", help="save confidences in --save-txt labels")
+    parser.add_argument("--save-ori-img", action="store_true", help="save original image")
     parser.add_argument("--nosave", action="store_true", help="do not save images/videos")
     parser.add_argument("--classes", nargs="+", type=int, help="filter by class: --class 0, or --class 0 2 3")
     parser.add_argument("--agnostic-nms", action="store_true", help="class-agnostic NMS")
@@ -315,6 +357,7 @@ if __name__ == "__main__":
     parser.add_argument("--exist-ok", action="store_true", help="existing project/name ok, do not increment")
     parser.add_argument("--no-trace", action="store_true", help="don`t trace model")
     parser.add_argument("--onlyball", action="store_true", help="plot only ball")
+    parser.add_argument("--mark-no-count", action="store_true", help="mark no count")
     parser.add_argument("--ball-top-boundary", default="", help="ball boundary")
     parser.add_argument("--ball-botton-boundary", default="", help="ball boundary")
     parser.add_argument("--person-left-boundary", default="", help="person boundary")
@@ -332,7 +375,7 @@ if __name__ == "__main__":
         yoloV7 = YoloV7()
         if opt.update:  # update all models (to fix SourceChangeWarning)
             for opt.weights in ["yolov7.pt"]:
-                yoloV7.detect(only_ball=opt.onlyball)
+                yoloV7.detect(save_ori_img=opt.save_ori_img, only_ball=opt.onlyball, mark_no_count=opt.mark_no_count)
                 strip_optimizer(opt.weights)
         else:
-            yoloV7.detect(only_ball=opt.onlyball)
+            yoloV7.detect(save_ori_img=opt.save_ori_img, only_ball=opt.onlyball, mark_no_count=opt.mark_no_count)
